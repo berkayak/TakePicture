af06dbafb54f781c4ab9a7f6b4afaf7ba8686e1a
berkayak <berkayak95@outlook.com>
2019-12-24 19:49:29 +0300

    initialize

---
+++ app/src/main/java/net/berkayak/takepicture/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.berkayak.takepicture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
